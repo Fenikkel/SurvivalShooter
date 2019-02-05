1f60560112588062661402d6438a2627f6f18297
Fenikkel <paufenollosa@gmail.com>
2019-02-05 09:56:19 +0100

    First enemy done

---
+++ SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset; //la distancia inicial entre el jugador y la camera
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    //FixedUpdate es para las fisicas, pero como movemos el jugador en fixedUpdate tambien lo haremos en la camera para que se mueva acorde
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+
+
+}
